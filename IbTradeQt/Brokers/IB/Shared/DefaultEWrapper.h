e490ad32fbe57646cc095858efa68fd4a9b97629
Denis Vodchyts <ctalgotrading@protonmail.com>
2023-01-12 09:26:11 +0100

    Added windows support for API 10.19
    reorganized folder structure
    added missing includes
    added intel lib x64 (debug) for windows

---
+++ IbTradeQt/Brokers/IB/Shared/DefaultEWrapper.h
@@ -0,0 +1,17 @@
+﻿/* Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
+
+#pragma once
+#ifndef TWS_API_CLIENT_DEFAULTEWRAPPER_H
+#define TWS_API_CLIENT_DEFAULTEWRAPPER_H
+
+#include "standardincludes.h"
+#include "EWrapper.h"
+
+class TWSAPIDLLEXP DefaultEWrapper :
+    public EWrapper
+{
+public:
+	#include "EWrapper_prototypes.h"
+};
+#endif
